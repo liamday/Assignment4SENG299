699c51ac9e317f598e7a547464fe18e81ad45274
Liam <Lime7029@gmail.com>
2017-07-14 16:13:15 -0700

    Update test_mothership_basic.py

--- ShillBot/tests/test_mothership_basic.py
+++ ShillBot/tests/test_mothership_basic.py
@@ -5,4 +5,15 @@ from mothership.base import MothershipServer
 
 
 class TestMothershipBasic(unittest.TestCase):
-    pass
+    
+    def test_URL_reachabillity(self):
+        """
+        Purpose: Test error if URL not reached
+        Expectation: startup system, Fail to find user (exception)
+        :precondition: URL not reachable
+        :return:
+        """
+        worker = BasicUserParseWorker("https://www.reddit.com/user/badLinkBadUser")
+
+        # Can't reach url
+        self.assertRaises(IOError, worker.run)
