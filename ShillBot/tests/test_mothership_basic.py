122bbceaccbada1288db570b2f8d9c3bf1d8b676
Liam <Lime7029@gmail.com>
2017-07-14 16:18:10 -0700

    Update test_mothership_basic.py

--- ShillBot/tests/test_mothership_basic.py
+++ ShillBot/tests/test_mothership_basic.py
@@ -6,14 +6,4 @@ from mothership.base import MothershipServer
 
 class TestMothershipBasic(unittest.TestCase):
     
-     def test_basic_worker_connection(self):
-        """
-        Purpose: Test regular running of worker
-        Expectation: startup system, hit the reddit user and parse the data, fail to send to mothership (exception)
-        :precondition: Mothership server not running
-        :return:
-        """
-        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-
-        # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
-        self.assertRaises(ConnectionRefusedError, worker.run)
+     pass
