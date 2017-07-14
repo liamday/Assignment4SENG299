910a03c6b1b6cc5f8dca1cfed0e5ab6cc5d39f3e
Liam <Lime7029@gmail.com>
2017-07-14 16:14:31 -0700

    Update test_worker_basic.py

--- ShillBot/tests/test_worker_basic.py
+++ ShillBot/tests/test_worker_basic.py
@@ -60,6 +60,17 @@ class TestWorkerBasic(unittest.TestCase):
 
         self.assertEqual(len_to_crawl_after, len_to_crawl_before)
 
+    def test_URL_reachabillity(self):
+        """
+        Purpose: Test error if URL not reached
+        Expectation: startup system, Fail to find user (exception)
+        :precondition: URL not reachable
+        :return:
+        """
+        worker = BasicUserParseWorker("https://www.reddit.com/user/badLinkBadUser")
+
+        # Can't reach url
+        self.assertRaises(IOError, worker.run)
 
 
 
